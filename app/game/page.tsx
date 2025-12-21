16bbe19f0af32ef11a6a1d18fc60e6b9c4927171
KenPTNK <khactrungkiennguyen@gmail.com>
2025-12-21 22:07:01 +0700

    Refactor GamePage to fetch games from Supabase and display them using GameCard; remove SearchBar component. (Finally lets go)

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,18 +1,51 @@
-import GameCard from '@/components/GameCard';
-import SearchBar from '@/components/SearchBar';
+'use client';
 
-export default function SearchPage() {
+import GameCard from "@/components/GameCard";
+import { supabase } from "@/src/supabaseClient";
+import { useState, useEffect } from "react";
+
+export default function GamePage() {
+  const [fetchError, setFetchError] = useState<string | null>(null);
+  const [games, setGames] = useState<Array<any> | null>(null);
+
+  useEffect(() => {
+    const fetchGames = async () => {
+      const { data, error } = await supabase.from("games").select();
+
+      if (error) {
+        setFetchError("Could not fetch the games");
+        setGames(null);
+        console.log(error);
+      }
+
+      if (data) {
+        setGames(data);
+        setFetchError(null);
+      }
+    };
+
+    fetchGames();
+  }, []);
+
   return (
     <div className="max-w-4xl mx-auto bg-white p-6 rounded shadow">
       <h2 className="text-2xl font-semibold mb-6">Search Games</h2>
 
-      <SearchBar />
-
-      <GameCard />
-
-      <button className="mt-6 w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700">
-        Search
-      </button>
+      {fetchError && <p className="text-red-500 mb-4">{fetchError}</p>}
+      {games && (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {games.map((game) => (
+            <GameCard
+              key={game.id}
+              name={game.name}
+              genre={game.genre}
+              platform={game.platform}
+              releaseYear={game.year}
+              rating={game.rating}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
